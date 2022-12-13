1f2755cbb6bc30f158537bb0fd5c16845315fd13
ad0maa <adam.tunchay@me.com>
2022-12-13 13:42:37 +1100

    addEntry moved to App()

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import ShowEntry from './ShowEntry'
 import Home from './Home'
 import NewEntry from './NewEntry'
 import Navbar from './Navbar'
-import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom'
+import { Routes, Route, useParams, useNavigate } from 'react-router-dom'
 
 const seedEntries = [
   {category: 'Food', content: 'Pizza is awesome!'},
@@ -16,28 +16,37 @@ const seedEntries = [
 
 const App = () => {
   const [entries, setEntries] = useState(seedEntries)
+  const nav = useNavigate()
 
   // HOC (higher-order component)
   const ShowEntryWrapper = () => {
     const { id } = useParams()
-    const entry = entries[id]
-    return entry ? <ShowEntry entry={entry} /> : <h4>Entry not Found!</h4>
+    const the_entry = entries[id]
+    return the_entry ? <ShowEntry entry={the_entry} /> : <h4>Entry not Found!</h4>
   }
 
 
+  const addEntry = (category, content) => {
+    const id = entries.length
+    // Add a new entry
+    const newEntry = {
+        category: category,
+        content: content
+    }
+    setEntries([...entries, newEntry])
+    nav(`/entry/${id}`)
+  }
+
   return (
     <>
-      <BrowserRouter>
       <Navbar />
        <Routes>
         <Route path='/' element={<Home entries={entries}/>} />
         <Route path='/category' element={<CategorySelection />} />
         <Route path = '/entry/:id' element={<ShowEntryWrapper />} />
-        <Route path='/entry/new/:category' element={<NewEntry entries= {entries} setEntries={setEntries}/>} />
+        <Route path='/entry/new/:category' element={<NewEntry addEntry={addEntry}/>} />
         <Route path='*' element = {<h4>Page not found!</h4>} />
        </Routes>
-      </BrowserRouter>
-
     </>
   )
 }
