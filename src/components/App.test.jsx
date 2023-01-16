175a963eac915eea2e3bcf12b0bbb0ab968dcebe
ad0maa <adam.tunchay@me.com>
2023-01-16 14:35:08 +1100

    pre delploy commit

--- src/components/App.test.jsx
+++ src/components/App.test.jsx
@@ -6,24 +6,24 @@ import { expect } from "vitest"
 import App from "./App"
 
 describe('App Component', () =>{
-    it('Shows the Journal Entries heading', () => {
-        const { container } = render(
-        <BrowserRouter>
-        <App />)
-        </BrowserRouter>
-    )
+    let container
 
+    beforeEach(function () {
+        container = render(
+            <BrowserRouter>
+            <App />)
+            </BrowserRouter>
+        ).container
+    })
+
+
+    it('Shows the Journal Entries heading', () => {
     expect(container.querySelector('h2')).toBeDefined()
     expect(container.querySelector('h2')).toHaveTextContent('Journal Entries')
     // expect(screen.getByRole('heading', {level:2})).toHaveTextContent('foo')
         })
 
     it('Shows category selection page when Select Category is clicked', async () => {
-        const { container } = render(
-            <BrowserRouter>
-            <App />)
-            </BrowserRouter>
-        )
 
         await userEvent.click(screen.getByText('Select Category'))
         expect(container.querySelector('h2')).toBeDefined()
